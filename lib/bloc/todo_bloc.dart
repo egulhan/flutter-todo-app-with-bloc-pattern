db8149e884ebcffbe1fd7c68bfb31ef39cfdef24
Erman Gülhan <mr.ermangulhan@gmail.com>
2020-08-06 00:39:35 +0300

    fixed typo

--- lib/bloc/todo_bloc.dart
+++ lib/bloc/todo_bloc.dart
@@ -6,7 +6,6 @@ import 'package:todo_app_with_bloc/data/todo_repository.dart';
 import 'package:todo_app_with_bloc/models/Todo.dart';
 
 part 'todo_event.dart';
-
 part 'todo_state.dart';
 
 class TodoBloc extends Bloc<TodoEvent, TodoState> {
